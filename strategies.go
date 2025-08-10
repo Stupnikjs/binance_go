4fc309b12c231d8572abb8315bc964c0e7ac19cc
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2025-08-10 21:16:56 +0200

    Update strategies.go

--- strategies.go
+++ strategies.go
@@ -7,27 +7,7 @@ import (
 	binance_connector "github.com/binance/binance-connector-go"
 )
 
-func EMAcrossOver(client *binance_connector.Client, pair string, EMAcoefShort int, EMAcoefLong int, over bool) bool {
-	klines := GetKlines(client, pair, "1m", EMAcoefLong*2)
-	closing := CloseFromKlines(klines)
-	EMAshort := ExponetialMovingAverage(closing, EMAcoefShort)
-	EMALong := ExponetialMovingAverage(closing, EMAcoefLong)
 
-	bullishCross := EMAshort[len(EMAshort)-2] < EMALong[len(EMALong)-2] && EMAshort[len(EMAshort)-1] > EMALong[len(EMALong)-1]
-	bearishCross := EMAshort[len(EMAshort)-2] > EMALong[len(EMALong)-2] && EMAshort[len(EMAshort)-1] < EMALong[len(EMALong)-1]
-	if over {
-		if bullishCross {
-			return true
-		}
-	}
-	if !over {
-		if bearishCross {
-			return true
-		}
-	}
-	return false
-
-}
 
 type StrategyStat struct {
 	Asset     string
