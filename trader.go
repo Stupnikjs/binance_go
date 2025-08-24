db9131db159e5582e7049b56e1475019e7e0203c
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2025-08-24 08:48:45 +0200

    Update trader.go

--- trader.go
+++ trader.go
@@ -7,7 +7,7 @@ var PAIRS = []string{
 type ITTrader interface {
 	Buy() error
 	Sell() error
-	LoopBuilder(s Strategy) func(klines *Klines, prevOver *bool, i int) (bool, error)
+	LoopBuilder(s Strategy) func(klines *Klines, prevOver *bool, i int) (bool, error) // change so it works with go routines
 	// We'll also add a method to check if the trade is over.
 	IsTradeOver() bool
 	GetGain() (error, float64)
