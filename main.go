144692058cb35973cc7eadd0cf33ff442a4197c9
stupnikk <andre.lacassagne82@gmail.com>
2025-08-25 22:10:07 +0200

    ok

--- main.go
+++ main.go
@@ -36,23 +36,27 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+	/*
+		testStrat := Strategy{
+			USDCAmount: 10,
+			Type:       "Cross over EMA",
+			Params: kli.IndicatorsParams{
+				Short_period_MA: 9,
+				Long_period_MA:  21,
+				RSI_coef:        14,
+				VROC_coef:       15,
+			},
+			Intervals: []kli.Interval{m5, m15, h1, h2},
+		}
 
-	testStrat := Strategy{
-		USDCAmount: 10,
-		Type:       "Cross over EMA",
-		Params: kli.IndicatorsParams{
-			Short_period_MA: 9,
-			Long_period_MA:  21,
-			RSI_coef:        14,
-			VROC_coef:       15,
-		},
-		Intervals: []kli.Interval{m5, m15, h1, h2},
-	}
+		_, err = testStrat.RunWrapper(client)
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	_, err = testStrat.RunWrapper(client)
-	if err != nil {
-		fmt.Println(err)
-	}
+	*/
+	kline := kli.BuildKlinesArr(client, "BTCUSDC", kli.Interv)
+	kli.AppendKlineToFile(*kline[0], "BTCUSDC", kli.Interv[0])
 
 	// Get API credentials from environment variable
 
--- pkg/klines/save.go
+++ pkg/klines/save.go
@@ -3,6 +3,7 @@ package klines
 import (
 	"encoding/gob"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
@@ -19,23 +20,24 @@ func SaveKline(pair string, interval Interval) error {
 	return nil
 }
 
-func SaveKlineToFile(kline Klines, pair string, interval Interval) error {
+func AppendKlineToFile(kline Klines, pair string, interval Interval) error {
 	fileName := fmt.Sprintf("%s_%s", pair, string(interval))
 	var data []binance_connector.KlinesResponse
 	for _, k := range kline.Array {
 		data = append(data, *k)
 	}
-	return SaveToFile(data, fileName)
+	return AppendToFile(data, fileName)
 }
 
-func SaveToFile(data []binance_connector.KlinesResponse, filename string) error {
-	file, err := os.Create(filename)
+// AppendToFile opens a file in append mode and encodes the new data to the end.
+// This is more efficient than reading the entire file, appending, and then saving.
+func AppendToFile(data []binance_connector.KlinesResponse, filename string) error {
+	// os.O_APPEND ensures we write to the end of the file.
+	// os.O_CREATE creates the file if it doesn't exist, which is a good practice.
+	// os.O_WRONLY is for write-only mode.
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		if err != os.ErrExist {
-			// call the append method
-			return nil
-		}
-		return fmt.Errorf("could not create file: %w", err)
+		return fmt.Errorf("could not open file for appending: %w", err)
 	}
 	defer file.Close()
 
@@ -46,18 +48,32 @@ func SaveToFile(data []binance_connector.KlinesResponse, filename string) error 
 	return nil
 }
 
-func loadKlinesFromFile(filename string) ([]binance_connector.KlinesResponse, error) {
+// loadKlinesFromFile has been updated to read multiple gob-encoded objects
+// from the file stream until it reaches the end of the file (io.EOF).
+func LoadKlinesFromFile(filename string) ([]binance_connector.KlinesResponse, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, fmt.Errorf("could not open file: %w", err)
 	}
 	defer file.Close()
 
-	var data []binance_connector.KlinesResponse
+	var allData []binance_connector.KlinesResponse
 	decoder := gob.NewDecoder(file)
-	if err := decoder.Decode(&data); err != nil {
-		return nil, fmt.Errorf("could not decode data: %w", err)
+
+	// Loop to decode all objects from the gob stream until an error or EOF is encountered.
+	for {
+		var data []binance_connector.KlinesResponse
+		if err := decoder.Decode(&data); err != nil {
+			// If we reach the end of the file, break the loop.
+			if err == io.EOF {
+				break
+			}
+			// For any other error, return the error.
+			return nil, fmt.Errorf("could not decode data: %w", err)
+		}
+		// Append the newly decoded data to the slice of all data.
+		allData = append(allData, data...)
 	}
 
-	return data, nil
+	return allData, nil
 }
